39af501da679bd290a3abeae5595084262e2f103
Amit Saini <amitascs@gmail.com>
2020-05-03 17:00:38 +0530

    Updated the code for Eureka client.

--- CustomerAPI/src/main/java/org/shopping/springbootstarter/CustomerApiApp.java
+++ CustomerAPI/src/main/java/org/shopping/springbootstarter/CustomerApiApp.java
@@ -2,12 +2,14 @@ package org.shopping.springbootstarter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Amit Saini
  *
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerApiApp {
 
 	/**
